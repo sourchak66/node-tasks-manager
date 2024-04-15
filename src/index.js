e660589e70576807e76d0dd75f3386b6bda36ff9
sourchak <sourav.ece9@gmail.com>
2024-04-15 10:33:49 +0530

    Initial Commit

--- src/index.js
+++ src/index.js
@@ -3,13 +3,16 @@ import userRouter from "./routers/user.js";
 import taskRouter from "./routers/task.js";
 import("./db/mongoose.js");
 
+
 const port = process.env.PORT || 3000;
 
 const app = express();
 
+
 app.use(express.json());
 app.use(userRouter.router);
 app.use(taskRouter.router);
+
 
 app.listen(port, () =>
 {
